5334af890bb1671fad8e799cc16757b66e316362
Arraytionary <kmkanokpon3@gmail.com>
2019-04-15 19:44:06 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <b-nav tabs>
+            <b-nav-item disabled><b>Fancy Quiz App</b></b-nav-item>
+            <b-nav-item disabled><b>Counter: {{numCorrect}}/{{numTotal}}</b></b-nav-item>
+        </b-nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header",
+        props: [
+            'numCorrect',
+            'numTotal'
+        ]
+    }
+</script>
+
+<style scoped>
+
+</style>
